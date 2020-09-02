c3d1c1778d3e803f0b7fea196c164ebac8d6eb22
Alberto <abettetto@gmail.com>
2020-09-01 22:59:32 -0500

    Add files - FundamentosJS

---
+++ FundamentosJSCurso/clase1.js
@@ -0,0 +1,17 @@
+console.log('Hola Javascript')
+
+// Declaracion de variable 
+var nombre = 'Alberto'
+var apellido = 'Bueno'
+var edad = '27'
+
+// Por convencion en javascript se usa la comilla simpre para agregar un string 
+
+nombre = 'Alberto'
+
+// Javascript no nos obliga a usar el ; 
+
+console.log('Hola '+ nombre + ' '+apellido)
+console.log('Tengo '+edad+' Años')
+
+// Javascript es un lenguaje debilmente tipado.
