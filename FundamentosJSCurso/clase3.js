5f30868bc602de5ebbae9c6a0feb424f2fc6b282
Alberto <abettetto@gmail.com>
2020-09-03 23:13:04 -0500

    Add files Class3 - Variables Numeros

---
+++ FundamentosJSCurso/clase3.js
@@ -0,0 +1,18 @@
+
+var edad = 27 
+
+// Incrementar el valor de uuna variable
+
+edad = edad + 1 // ó 
+edad += 1
+
+var peso = 75 
+peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+
+peso = peso + sandwich
+
+var jugarAlFutbol = 3
+peso = peso - jugarAlFutbol
