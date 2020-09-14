7a84d9bc5be9a43bce69770a35c02e1cfad0435d
Alberto <abettetto@gmail.com>
2020-09-13 23:58:35 -0500

    Modified index.js

--- ecmascript/es6/index.js
+++ ecmascript/es6/index.js
@@ -1,3 +1,5 @@
+/* Con la inclusion de es6 podemos escribir de forma diferente las funciones */
+
 function newFunction (name, age, country){
     var name = name || `Alberto`
     var age = age || 32
@@ -19,3 +21,40 @@ let epic = hola + ` `+ world
 console.log(epic)
 let epic2 = `${hola} ${world}`
 console.log(epic2)
+
+/* Multilinea en los String*/
+
+// Asi lo haciamos anteriormente 
+let lorem = `Una frase epica que separabamos el un \n` + `segundo reglon una frase`
+
+//con es6 
+let lorem2= `Utilizando las comillas francesas podemos utilizar una frese
+con un segundo saldo de linea con es6`
+
+console.log(lorem)
+console.log(lorem2)
+
+
+/* DESESTRUCTURACION */
+
+let person = {
+    'name' : "Alberto",
+    'age' : 32,
+    'country' : "PA"
+}
+console.log(person.name, person.age)
+
+let {name, age, country} = person
+
+console.log(name, age, country)
+
+/* OPERADOR DE PROPAGACION */
+// Permite unir dos array y cada uno de sus elementos usando ...para agregar cada elemento
+let team1 = ['Oscar', 'Julian', 'Ricardo']
+let team2 = ['Caro', 'Isis', 'Andrea']
+
+let education = ['david', ...team1, ...team2]
+
+console.log(education)
+
+
